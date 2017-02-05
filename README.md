88cecd4db6eb21751a181e5af3f98552a06c986f
omarchaeopteryx <omaralimalik@gmail.com>
2017-02-04 17:52:52 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+// Feb. 2017.
+
+This is a Swift sandbox for learning purposes. It's not terribly exciting at the moment, but I'm hoping it grows into something more meaningful.
---
+++ hello_world.swift
@@ -0,0 +1,19 @@
+// February 2017. OM.
+
+func sayHello(_ name: String) {
+
+	print("\nHello there, \(name)!\n")
+
+}
+
+var x = "Zarathustra"
+var y = "Zoroaster"
+var z = "Nietzsche"
+
+let nameArray = [ x, y, z ]
+
+for i in nameArray {
+
+	sayHello(i)
+
+}
